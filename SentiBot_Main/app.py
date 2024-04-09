193aa52bd9520f0ca7a2e05658cddbc3bcd89b75
Aditya-009 <technoadityaraj@gmail.com>
2024-04-09 19:35:06 +0530

    initial chatbot added

--- SentiBot_Main/app.py
+++ SentiBot_Main/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,8 +35,37 @@ def video():
     return render_template('video.html')
 
 @app.route('/chat')
-def chat():
+def chat_page():
     return render_template('chat.html')
+
+@app.route("/get", methods=["POST"])
+def chat():
+    global chat_history_ids
+    
+    if request.method == "POST":
+        # Get user message from the request form
+        user_message = request.form["msg"]
+        
+        # Generate response from chatbot model
+        response = get_Chat_response(user_message)
+        
+        return jsonify({'user_message': user_message, 'chatbot_response': response})
+    
+def get_Chat_response(text):
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 
 @app.route('/process_audio_video', methods=['POST'])
